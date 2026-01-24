74da66d7e8bd8580cdd7c9611feeac0d16788c95
dingjing <dingjing@live.cn>
2026-01-24 15:39:24 +0800

    Test 十六进制数组转字符串

--- tests/test-str.c
+++ tests/test-str.c
@@ -28,6 +28,15 @@ int main (int argc, char* argv[])
 {
     printf("Start test....\n");
 
+    printf("十六进制转为字符串");
+    {
+        const char hex[] = {0x01, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+        char dest[1024] = {0};
+        c_utils_str_hex2str((uint8_t*) dest, (uint8_t*) hex, C_ARRAY_COUNT(hex));
+        printf("%s\n", dest);
+    }
+
+    printf("获取路径中的 文件夹 和 目录\n");
     {
         const char* fullPath = "/a/b/c/d.txt";
         char dirPath[1024] = {0};
